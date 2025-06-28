cee4e8ca152d447d243576cf5ef9967b1bc1c3c8
aviej20 <90211835+aviej20@users.noreply.github.com>
2025-06-28 14:26:29 -0500

    Small js addition & general styling updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-var navItem = document.querySelector(".navbar").childNodes;
-
-console.log(navItem);
+if(window.matchMedia("(max-width: 768px)").matches){
+   const heading= document.querySelector(".heading");
+   heading.textContent = "Alejandro Viejo";
+}
