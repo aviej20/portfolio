df25893da144374dd4205bf48e36b0bc81898418
aviej20 <90211835+aviej20@users.noreply.github.com>
2025-07-13 13:31:31 -0500

    Updated mobile styling

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-if(window.matchMedia("(max-width: 768px)").matches){
+if(window.matchMedia("(max-width: 1024px)").matches){
    const heading= document.querySelector(".heading");
    heading.textContent = "Alejandro Viejo";
+
+   const projectCardPreviews = document.querySelectorAll('.project-card--preview');
+   const projectCardDescriptions = Array.from(document.querySelectorAll('.project-card--description'));
+
+   projectCardPreviews.forEach((element, index) => {
+      projectCardDescriptions[index].appendChild(element);
+      element.querySelector('.project-card--img').setAttribute('height', '350');
+   });
 }
